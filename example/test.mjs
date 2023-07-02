a02f005d8db28b006e2486a218f1640aed7981f5
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-07-02 21:50:48 +0800

    test

--- example/test.mjs
+++ example/test.mjs
@@ -2,3 +2,16 @@
 import { Client } from '@falloutstudios/sourcebin.js';
 
 const sourcebin = new Client();
+
+
+const bin = await sourcebin.createBin({
+    title: 'Test',
+    files: [
+        {
+            name: 'test.txt',
+            content: 'console.log(`Hellow, world!`)'
+        }
+    ]
+});
+
+console.log(bin);
